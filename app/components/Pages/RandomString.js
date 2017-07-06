6f327072bdd7cb82cb47e8a70d4272204223dec4
wangmingfeng1 <wangmingfeng1@wanda.cn>
2017-07-06 15:23:24 +0800

    :heavy_plus_sign: 添加node-sass

---
+++ app/components/Pages/RandomString.js
@@ -0,0 +1,27 @@
+import React, {Component, PropTypes} from "react"
+import {connect} from "react-redux";
+import {TextField, RaisedButton} from 'material-ui';
+import './style/RandomString.css';
+
+export default class RandomString extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    render() {
+        return (
+            <div className="selfWrap">
+                1111
+            </div>
+        )
+    }
+}
